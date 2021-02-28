8bf67866dba60b62081f3e5373630efb442a2dca
Ehtasham <ehtashamiiuc42@gmail.com>
2021-02-28 21:16:48 +0600

    half

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logo} alt="" />
+            <nav><a href="/shop">Shop</a>
+                <a href="/review">Order review</a>
+                <a href="/manage">Manage Inventory here</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+const Product = (props) => {
+    const { img, name, seller, price, stock } = props.product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by :{seller}</small></p>
+                <p>${price}</p>
+                <p><small>only {stock} left in the stock - Order soon.</small></p>
+                <button className="main-button"
+                onClick={()=>props.handleAddProduct(props.product)}
+                > <FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
